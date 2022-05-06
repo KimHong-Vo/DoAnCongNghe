169085207689cf4998793f3f811ef529ae724787
dangkiet2000 <66420928+dangkiet2000@users.noreply.github.com>
2022-05-06 11:05:35 +0700

    set up navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './sass/index.scss';
 import './App.css';
+import NavigationBar from './components/Shared/NavigationBar/NavigationBar';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
       <h1>Quản Lí Công Việc</h1>
     </div>
   );
