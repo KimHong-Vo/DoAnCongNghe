0d19368a33817c7b91e900129d98b0730d66f7ee
KimHong-Vo <18130087@st.hcmuaf.edu.vn>
2022-05-19 22:34:49 +0700

    update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import TableCreatingPopup from './components/Shared/TableCreatingPopup/TableCreatingPopup';
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
-import ReactDOM from 'react-dom/client';
+// import App from './App'
+// import ReactDOM from 'react-dom/client';
 // import App from './App';
 // import WorlSpaceCreatingPopup from './components/Shared/WorlSpaceCreatingPopup/WorlSpaceCreatingPopup';
 import TablesOfWorkSpace from './pages/Home/TablesOfWorkSpace/TablesOfWorkSpace';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-    {/* <WorlSpaceCreatingPopup /> */}
     <TablesOfWorkSpace/>
-  </React.StrictMode>
+  </React.StrictMode>, document.getElementById('root')
 );
    
