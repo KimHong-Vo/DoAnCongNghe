24bd139035b31a870b4b542330cbfe0413ef2440
KimHong-Vo <18130087@st.hcmuaf.edu.vn>
2022-05-19 22:19:04 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 import TableCreatingPopup from './components/Shared/TableCreatingPopup/TableCreatingPopup';
-import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <TableCreatingPopup></TableCreatingPopup>
-  </React.StrictMode>
-);
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
+   
