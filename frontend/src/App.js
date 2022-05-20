137d9c9ba56e0ebc05a89d576aa8a5532e6fe22d
KimHong-Vo <18130087@st.hcmuaf.edu.vn>
2022-05-20 09:39:18 +0700

    update sprint1 full

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,23 @@
 
 import Menu from './components/Shared/Menu/Menu'
-
-function App() {
-  return (
-    <div className="main">
-      <Menu />
-      <div className="container">
-        <h1 className="title">My React App</h1>
-        <p className="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className="btn">Explore now</button>
-      </div>
 import './App.scss';
 import NavigationBar from './components/Shared/NavigationBar/NavigationBar';
-
 function App() {
   return (
+    // <div className="main">
+    //   <Menu />
+    //   <div className="container">
+    //     <h1 className="title">My React App</h1>
+    //     <p className="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    //     <button className="btn">Explore now</button>
+    //   </div>
+    // </div>
+
     <div className="App">
       <NavigationBar />
       <h1>Quản Lí Công Việc</h1>
     </div>
   )
-    // <div className='container'>
-    //   <div className='item'>Hello</div>
-    // </div>
-    // <div className="App">
-    //   <h1>Quản Lí Công Việc</h1>
-    // </div>
-    
-  // );
 }
 
 export default App
