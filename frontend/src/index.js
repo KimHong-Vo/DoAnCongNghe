9b7631812d6538e17c2661b01b9cf9fc55b86cd5
dangkiet2000 <66420928+dangkiet2000@users.noreply.github.com>
2022-05-22 21:13:07 +0700

    done

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from "react-router-dom";
 // import TableCreatingPopup from './components/Shared/TableCreatingPopup/TableCreatingPopup';
 // import TablesOfWorkSpace from './pages/Home/TablesOfWorkSpace/TablesOfWorkSpace';
 // import WorlSpaceCreatingPopup from './components/Shared/WorlSpaceCreatingPopup/WorlSpaceCreatingPopup'
@@ -10,7 +11,9 @@ import './App.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
