54555bde13b3d2dfd7b7c64bcafd873cef6533fe
17130068 <17130068@st.hcmuaf.edu.vn>
2022-05-30 07:47:45 +0700

    WSRequest

--- frontend/src/components/Shared/WorlSpaceCreatingPopup/WorlSpaceCreatingPopup.jsx
+++ frontend/src/components/Shared/WorlSpaceCreatingPopup/WorlSpaceCreatingPopup.jsx
@@ -4,27 +4,35 @@ import Background from './CreateWSpaceBground.png'
 import WSicon from './CreateWspacePic.png'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import {useState} from 'react';
 
 const WorlSpaceCreatingPopup = () => {
+  const [wsName,setWSName] = useState('');
+  const [wsType,setWSType] = useState('');
+  const [description,setDescription] = useState('');
+  const handleSubmitBtn = (e) => {
+    e.preventDefault();
+    console.log('>>>check data state: ', wsName,wsType,description);
+  }
   return (
     <Box sx={{ flexGrow: 1 }} className = 'popupBox'>
       <Grid className='container' container columns={{ xs: 4, sm: 8, md: 12 }}>
           <Grid item xs={4} sm={8} md={8} key={0} className= 'leftGrid'>
             <div className='leftItem'>
-              <div className='title'>
+              {/* <div className='title'>
                 <h2>Let buil a work space</h2>
                 <h4>Increating your productive by helping everyone can easyly access to tables in workspace</h4>
-              </div>
+              </div> */}
               <div className='form'>
-                <form action="submit" className='createWSForm'>
+                <form className='createWSForm' onSubmit={handleSubmitBtn}>
                   <div className='wsName'>
                     <p htmlFor='wsName'>Work space name*</p>
-                    <input type='text' name="wsName" id="wsName" placeholder='Big Brain bussiness'/>
+                    <input type='text' name="wsName" id="wsName" value={wsName} onChange={(e)=>setWSName(e.target.value)} placeholder='Big Brain bussiness' />
                     <p className='fiedExplain'>This is your bussiness name, group or organization</p>
                   </div>
                   <div className='wsType'>
                     <p>Type of work space*</p>
-                    <select>
+                    <select value={wsType} onChange={(e)=>setWSType(e.target.value)}>
                       <option value="" disabled selected hidden>Choose :</option>
                       <option value="1">Marketing</option>
                       <option value="2">Small business</option>
@@ -36,10 +44,10 @@ const WorlSpaceCreatingPopup = () => {
                   </div>
                   <div className='description'>
                     <p>Decrible work space</p>
-                    <textarea name="" id="" rows="6" placeholder='Our group organize every thing here.'></textarea>
+                    <textarea value={description} onChange={(e)=>setDescription(e.target.value)} name="" id="descriptionText" rows="6" placeholder='Our group organize every thing here.' />
                     <p className='fiedExplain'>Take your member into tables with some short description of work space</p>
                   </div>
-                  <button>Continue</button>
+                  <button type="submit">Continue</button>
                 </form>
               </div>
             </div>
