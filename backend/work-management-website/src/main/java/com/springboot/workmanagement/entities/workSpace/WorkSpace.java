9b600eb0b9874d3108eae403ada92c982bb0829f
KimHong-Vo <18130087@st.hcmuaf.edu.vn>
2022-06-17 22:05:02 +0700

    update after merge 24
    - two branch still not merger

--- backend/work-management-website/src/main/java/com/springboot/workmanagement/entities/workSpace/WorkSpace.java
+++ backend/work-management-website/src/main/java/com/springboot/workmanagement/entities/workSpace/WorkSpace.java
@@ -9,69 +9,13 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="WorkSpace")
-public class WorkSpace{
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private String workspace_name;
-	private String nameTable;
-	private int idUser;
-	private String logo;
-	private String typeOfWorkSpace;
-	private String descriptionOfWorkSpace;
-	public WorkSpace() {
-		super();
-	}
-	public WorkSpace(String workspace_name, String nameTable, int idUser, String logo,String typeOfWorkSpace, String descriptionOfWorkSpace) {
-		super();
-		this.workspace_name = workspace_name;
-		this.nameTable = nameTable;
-		this.idUser = idUser;
-		this.logo = logo;
-		this.typeOfWorkSpace = typeOfWorkSpace;
-		this.descriptionOfWorkSpace = descriptionOfWorkSpace;
-	}
-	public String getWorkspace_name() {
-		return workspace_name;
-	}
-	public void setWorkspace_name(String workspace_name) {
-		this.workspace_name = workspace_name;
-	}
-	public String getNameTable() {
-		return nameTable;
-	}
-	public void setNameTable(String nameTable) {
-		this.nameTable = nameTable;
-	}
-	public int getIdUser() {
-		return idUser;
-	}
-	public void setIdUser(int idUser) {
-		this.idUser = idUser;
-	}
-	public String getLogo() {
-		return logo;
-	}
-	public void setLogo(String logo) {
-		this.logo = logo;
-	}
-	public String getTypeOfWorkSpace() {
-		return typeOfWorkSpace;
-	}
-	public void setTypeOfWorkSpace(String typeOfWorkSpace) {
-		this.typeOfWorkSpace = typeOfWorkSpace;
-	}
-	public String getDescriptionOfWorkSpace() {
-		return descriptionOfWorkSpace;
-	}
-	public void setDescriptionOfWorkSpace(String descriptionOfWorkSpace) {
-		this.descriptionOfWorkSpace = descriptionOfWorkSpace;
-
 public class WorkSpace implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
 	private String description;
+	private String logoPath;
 	
 	public WorkSpace() {}
 	public WorkSpace(String newName, String newDescription) {
@@ -81,20 +25,33 @@ public class WorkSpace implements Serializable{
 
 	}
 	
+	public int getId() {
+		return id;
+	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	public int getId() {
-		return this.id;
+	public String getName() {
+		return name;
 	}
-
-	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLogoPath() {
+		return logoPath;
+	}
+	public void setLogoPath(String logoPath) {
+		this.logoPath = logoPath;
+	}
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return "id: " + this.id + "- name: " + this.name + "- description: " + this.description;
 	}
-	
-	
 }
