ff2bb1c5e3cce7cfd4285e748a310e37e16860b8
18130039 <80388171+18130039@users.noreply.github.com>
2022-06-17 22:57:51 +0700

    upload handle request create workspace

---
+++ backend/work-management-website/src/main/java/com/springboot/workmanagement/service/workSpace/WorkSpaceService.java
@@ -0,0 +1,7 @@
+package com.springboot.workmanagement.service.workSpace;
+
+import com.springboot.workmanagement.entities.workSpace.WorkSpace;
+
+public interface WorkSpaceService {
+	public WorkSpace saveWorkSpace(WorkSpace workSpace);
+}
