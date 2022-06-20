e585565d855553efd0a9048234ce238d7b56a74b
18130039 <80388171+18130039@users.noreply.github.com>
2022-06-20 13:24:12 +0700

    remove import not use

--- backend/work-management-website/src/main/java/com/springboot/workmanagement/controllers/workSpace/WorkSpaceController.java
+++ backend/work-management-website/src/main/java/com/springboot/workmanagement/controllers/workSpace/WorkSpaceController.java
@@ -1,47 +1,40 @@
 package com.springboot.workmanagement.controllers.workSpace;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.workmanagement.entities.workSpace.WorkSpace;
-import com.springboot.workmanagement.repositories.workSpace.WorkSpaceRepository;
 import com.springboot.workmanagement.service.workSpace.WorkSpaceService;
 
 @RestController
-public class WorkSpaceController{
+public class WorkSpaceController {
 	@Autowired
 	WorkSpaceService workSpaceService;
 
-	
 	@GetMapping("/insertWorkspace")
 	public String insertWorkspace(@RequestBody WorkSpace workSpace) {
 		workSpaceService.saveWorkSpace(workSpace);
 		return "success";
 	}
-	
-//	@GetMapping("/getWorkspace")
-//	public String getWorkspace() {
-//		ObjectMapper mapper = new ObjectMapper();
-//		try {
-//			List<WorkSpace> workspaceList = new ArrayList<>();
-//			Iterable<WorkSpace> wss = workSpaceRepository.findAll();
-//			for(WorkSpace ws : wss) {
-//				workspaceList.add(ws);
-//			}
-//			return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(workspaceList);
-//		} catch (JsonProcessingException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			return "fail to read data";
-//		}
-//		
-//	}
+
+	// @GetMapping("/getWorkspace")
+	// public String getWorkspace() {
+	// ObjectMapper mapper = new ObjectMapper();
+	// try {
+	// List<WorkSpace> workspaceList = new ArrayList<>();
+	// Iterable<WorkSpace> wss = workSpaceRepository.findAll();
+	// for(WorkSpace ws : wss) {
+	// workspaceList.add(ws);
+	// }
+	// return
+	// mapper.writerWithDefaultPrettyPrinter().writeValueAsString(workspaceList);
+	// } catch (JsonProcessingException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// return "fail to read data";
+	// }
+	//
+	// }
 }
--- backend/work-management-website/src/main/java/com/springboot/workmanagement/entities/workSpace/WorkSpace.java
+++ backend/work-management-website/src/main/java/com/springboot/workmanagement/entities/workSpace/WorkSpace.java
@@ -5,43 +5,51 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-public class WorkSpace implements Serializable{
+public class WorkSpace implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String description;
 	private String logoPath;
-	
-	public WorkSpace() {}
-	
+
+	public WorkSpace() {
+	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public String getLogoPath() {
 		return logoPath;
 	}
+
 	public void setLogoPath(String logoPath) {
 		this.logoPath = logoPath;
 	}
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
