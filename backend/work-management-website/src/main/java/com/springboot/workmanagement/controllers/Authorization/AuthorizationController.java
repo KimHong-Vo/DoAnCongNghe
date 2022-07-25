a7f24714b18b073da1c248068f2ebdb90de51d15
KimHong-Vo <18130087@st.hcmuaf.edu.vn>
2022-07-25 11:24:08 +0700

    edit merge(back)

--- backend/work-management-website/src/main/java/com/springboot/workmanagement/controllers/Authorization/AuthorizationController.java
+++ backend/work-management-website/src/main/java/com/springboot/workmanagement/controllers/Authorization/AuthorizationController.java
@@ -3,6 +3,7 @@ package com.springboot.workmanagement.controllers.Authorization;
 import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,9 +41,9 @@ public class AuthorizationController {
 			ujwt.setUser(result);
 			authService.saveJWT(ujwt);
 			return new ResponseEntity<String>(token, HttpStatus.OK);
-		}
-	UserRepository repository;
-	Iterable<User> users;
+		}}
+//	UserRepository repository;
+//	Iterable<User> users;
 	// má»Ÿ form Ä‘Äƒng nháº­p
 	// khai bÃ¡o Ä‘Æ°á»�ng dáº«n Ä‘áº¿n trang Ä‘Äƒng nháº­p
 
@@ -62,22 +63,22 @@ public class AuthorizationController {
 
 	// xu ly dang nhap
 	// https://localhost:8080/account/login
-	@CrossOrigin
-	@PostMapping("/login")
-	public User logIn(@RequestBody User u) {
-		System.out.print("Login: ");
-		users = repository.findAll();
-		for (User user : users) {
-			if (user.equals(u)) {
-				System.out.println("success");
-				return user;
-				
-			}
-		}
-		System.out.println("fail");
-		return null;
-//		System.out.println("Hello");
-	}
+//	@CrossOrigin
+//	@PostMapping("/login")
+//	public User logIn(@RequestBody User u) {
+//		System.out.print("Login: ");
+//		users = repository.findAll();
+//		for (User user : users) {
+//			if (user.equals(u)) {
+//				System.out.println("success");
+//				return user;
+//				
+//			}
+//		}
+//		System.out.println("fail");
+//		return null;
+////		System.out.println("Hello");
+//	}
 	
 	@GetMapping("/getUser")
 	public ResponseEntity<User> getUserByToken(@RequestHeader("Authorization") String token) {
