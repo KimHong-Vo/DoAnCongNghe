17c593f8c62b110655bd97d08d91353289b538dd
DoanThuc1607 <74177802+18130233@users.noreply.github.com>
2022-07-25 21:36:10 +0700

    loadBoardForWorkspace

--- backend/work-management-website/src/main/java/com/springboot/workmanagement/repositories/workSpace/WorkspaceService.java
+++ backend/work-management-website/src/main/java/com/springboot/workmanagement/repositories/workSpace/WorkspaceService.java
@@ -1,11 +1,14 @@
 package com.springboot.workmanagement.repositories.workSpace;
 
 import java.util.List;
+import java.util.Set;
 
+import com.springboot.workmanagement.entities.workSpace.Table;
 import com.springboot.workmanagement.entities.workSpace.WorkSpace;
 
 public interface WorkspaceService {
 	WorkSpace saveWorkSpace(WorkSpace workSpace);
 	List<WorkSpace> getListWorkspace();
+	Set<Table> getListTableForWorkspace(Integer workspaceId);
 
 }
